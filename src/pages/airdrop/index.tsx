95d1e4d161c0270e84fac6b7f4f46705003f803b
Johnny <vult.pnv@gmail.com>
2022-02-14 17:11:46 +0700

    update: Update Main Page and Airdrop Page

--- src/pages/airdrop/index.tsx
+++ src/pages/airdrop/index.tsx
@@ -1,41 +1,34 @@
 import { FC } from 'react';
-import portfolioList from '../../portfolio/portfolio.json';
+import airdrops from '../../data/airdrops.json';
 
 export const AirdropPage: FC<any> = (props) => (
   <div className="home">
     <section id="portfolio">
       <div className="container">
-        <h3>Portfolio</h3>
+        <h3>Cosmos Airdrops</h3>
         <div className="row">
-          {portfolioList.map((e, i) => (
+          {airdrops.map((e, i) => (
             <div key={i} className="col-md-6 col-lg-6 col-xl-4">
-              <a href={e.link} target="_blank" rel="noreferrer">
-                <div className={`card ${e.name.length > 8 ? 'pr-0' : ''}`}>
-                  <div className="card-logo"><img height={90} width={90} src={require(`../../assets/icons/${e.logo}`)} alt="" /></div>
+              <a href={e.link_refer} target="_blank" rel="noreferrer">
+                <div className="card">
+                  <div className="card-logo"><img height={90} width={90} src={require(`../../assets/icons/${e.icon}`)} alt="" /></div>
                   <div className="card-body">
-                    <h5 className="card-title">{e.name}</h5>
+                    <h5 className="card-title">{e.project_name}</h5>
                     <div className="card-status">
                       <div className="status-h">Status</div>
                       <div className="status">{e.status}</div>
+                      <div className="status-h">Snapshot</div>
+                      <div className="status">{e.snapshot}</div>
+                      <div className="status-h">Claim Start</div>
+                      <div className="status">{e.claim_start}</div>
+                      <div className="status-h">Claim End</div>
+                      <div className="status">{e.claim_end}</div>
                     </div>
                   </div>
                 </div>
               </a>
             </div>
           ))}
-          <div className="col-md-6 col-lg-6 col-xl-4">
-            <a href="https://t.me/tcnetworkannoucement" target="_blank" rel="noreferrer">
-              <div className="card">
-                <div className="card-logo"><img height={46} width={46} src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0NiIgaGVpZ2h0PSI0NiI+CiAgICA8cGF0aCBmaWxsPSIjRkRGRUZGIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik00My41IDI2SDI1djE3LjVhMi41IDIuNSAwIDEgMS01IDBWMjZIMi41YTIuNSAyLjUgMCAxIDEgMC01SDIwVjIuNWEyLjUgMi41IDAgMSAxIDUgMFYyMWgxOC41YTIuNSAyLjUgMCAxIDEgMCA1eiIvPgo8L3N2Zz4K" alt="" /></div>
-                <div className="card-body">
-                  <h5 className="card-title">Contact Us</h5>
-                  <p className="card-text">
-                    if you’re looking for a validator
-                  </p>
-                </div>
-              </div>
-            </a>
-          </div>
         </div>
       </div>
     </section>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,7 +6,7 @@ import EducationIcon from '../../assets/icons/education.svg';
 import AlignmentIcon from '../../assets/icons/alignment.svg';
 import TransparencyIcon from '../../assets/icons/transparency.svg';
 import IntegrityIcon from '../../assets/icons/integrity.svg';
-import portfolioList from '../../portfolio/portfolio.json';
+import portfolioList from '../../data/portfolio.json';
 
 export const HomePage: FC<any> = (props) => (
   <div className="home">
@@ -55,33 +55,27 @@ export const HomePage: FC<any> = (props) => (
         <div className="row">
           {portfolioList.map((e, i) => (
             <div key={i} className="col-md-6 col-lg-6 col-xl-4">
-              <a href={e.link} target="_blank" rel="noreferrer">
+              <a href={e.home_page} target="_blank" rel="noreferrer">
                 <div className={`card ${e.name.length > 8 ? 'pr-0' : ''}`}>
+                  <div className="badge-corner-right"><h5><span id="badge-apy-cosmos" className="badge badge-pill badge-success badge-font">% APR</span></h5></div>
+                  <div className="badge-corner-left"><h5><span id="badge-price-cosmos" className="badge badge-dark badge-font">$Price</span></h5></div>
                   <div className="card-logo"><img height={90} width={90} src={require(`../../assets/icons/${e.logo}`)} alt="" /></div>
                   <div className="card-body">
                     <h5 className="card-title">{e.name}</h5>
                     <div className="card-status">
                       <div className="status-h">Status</div>
                       <div className="status">{e.status}</div>
-                    </div>
+                      </div>
+                     
+                  </div>
+                  <div className='staking'>
+                     <p className="achieve" id="cosmos_token"><img src={require(`../../assets/icons/staked.png`)} alt="" />999,999</p>
+                     <a className="boxed-btn" href={e.link}>Stake Now</a>
                   </div>
                 </div>
               </a>
             </div>
           ))}
-          <div className="col-md-6 col-lg-6 col-xl-4">
-            <a href="https://t.me/tcnetworkannoucement" target="_blank" rel="noreferrer">
-              <div className="card">
-                <div className="card-logo"><img height={46} width={46} src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0NiIgaGVpZ2h0PSI0NiI+CiAgICA8cGF0aCBmaWxsPSIjRkRGRUZGIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik00My41IDI2SDI1djE3LjVhMi41IDIuNSAwIDEgMS01IDBWMjZIMi41YTIuNSAyLjUgMCAxIDEgMC01SDIwVjIuNWEyLjUgMi41IDAgMSAxIDUgMFYyMWgxOC41YTIuNSAyLjUgMCAxIDEgMCA1eiIvPgo8L3N2Zz4K" alt="" /></div>
-                <div className="card-body">
-                  <h5 className="card-title">Contact Us</h5>
-                  <p className="card-text">
-                    if you’re looking for a validator
-                  </p>
-                </div>
-              </div>
-            </a>
-          </div>
         </div>
       </div>
     </section>
