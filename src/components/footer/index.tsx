287746f7cb863738ba276a1deeab9f96c25851da
trulacgc <trulacgc.tcnetwork@gmail.com>
2022-12-26 00:10:03 +0700

    Update email, github, staking link

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,15 +6,16 @@ export const Footer: FC<any> = (props) => (
     <div className="footer">
       <div className="container d-flex flex-column flex-md-row justify-content-between align-items-center">
         <div className="navbar-brand" >
-          <img src={LogoIcon} width={41} height={38} alt="" className="d-inline-block align-top" style={{ borderRadius: '100%', border: '1px solid #e19605' }} />
+          <img src={LogoIcon} width={41} height={38} alt="" className="d-inline-block align-top mr-1" style={{ borderRadius: '100%', border: '1px solid #e19605' }} />
           TC NETWORK
         </div>
         <div className="based">
-          Be part of the future!
+
         </div>
         <ul className="nav social">
-          <li><a href="https://t.me/tcnetworkannoucement" target="_blank" rel="noopener noreferrer" aria-label="Telegram"><i className="icon icon-telegram" /></a></li>
+          <li><a href="https://t.me/tcnetwork_io" target="_blank" rel="noopener noreferrer" aria-label="Telegram"><i className="icon icon-telegram" /></a></li>
           <li><a href="https://twitter.com/TCNetwork_io" target="_blank" rel="noopener noreferrer" aria-label="Twitter"><i className="icon icon-twitter" /></a></li>
+          <li><a href="https://github.com/tcnetworkio" target="_blank" rel="noopener noreferrer" aria-label="GitHub" className='icon icon-github'><i className="" /></a></li>
         </ul>
       </div>
     </div>
